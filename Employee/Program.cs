57528b878a5adc6334b0a572408936d9d20fd4fb
Hasan Gökalp <132149740+HasanGokalp@users.noreply.github.com>
2023-06-19 15:11:20 +0300

    Update Program.cs

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Employee.Models;
 using Employee.Models.Abstract;
 
@@ -10,15 +10,15 @@ namespace Employee
         {
             BasePersonel BP = new KadroluPersonel(1, "GOKALP","Hasan",3000,3);
             int aylik = BP.AylikHesapla();
-            Console.WriteLine(aylik.ToString());
-            
+            Console.WriteLine("Adi Soyadi : " + BP.AdSoyadGetir() + " Maasi " + aylik.ToString());
+
             BasePersonel BP2 = new SozlesmeliPersonel(2, "IMAL","Muhammed",13000,5);
             int aylik2 = BP2.AylikHesapla();
-            Console.WriteLine(aylik2.ToString());
+            Console.WriteLine("Adi Soyadi : " + BP2.AdSoyadGetir() + " Maasi " + aylik2.ToString());
             
             BasePersonel BP3 = new StajyerPersonel(3, "CETIN","Sinan",7000,7);
             int aylik3 = BP3.AylikHesapla();
-            Console.WriteLine(aylik3.ToString());
+            Console.WriteLine("Adi Soyadi : " + BP3.AdSoyadGetir() + " Maasi " + aylik3.ToString());
         }
     }
 }
